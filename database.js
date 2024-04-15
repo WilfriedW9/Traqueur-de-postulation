dff303360754a56363631083a8b5735f52fb6058
WilfriedW9 <zalo.Timothe@gmail.com>
2024-04-15 19:55:02 +0200

    Création du .env

--- database.js
+++ database.js
@@ -1,17 +1,35 @@
 // Ce fichier sert a établir une connexion entre mySql et Node
 import mysql from "mysql2"
+import dotenv from "dotenv"
+dotenv.config()
 
 const connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"DataBased_777!NeJxHw",
-    database:"ctracker1",
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     // Le port ici et le port de la base de données doivent correspondrent, sinon il y aura une erreur
-    port: 3006
+    port: process.env.MYSQL_PORT
 }).promise()
 
-const result = await connection.query("SELECT * FROM ctracker1.candidature")
-console.log(result[0])
+
+async function getCandidatures(){
+    const [rows] = await connection.query("SELECT * FROM ctracker1.candidature")
+    return rows
+}
+
+async function getCandidature(id){
+    const [rows] = await connection.query(`
+    SELECT * 
+    FROM ctracker1.candidature
+    WHERE CandidatureID = ?
+    `, [id])
+    return rows
+}
+
+const candidatures =  await getCandidature(1)
+
+console.log(candidatures)
 
 
 
